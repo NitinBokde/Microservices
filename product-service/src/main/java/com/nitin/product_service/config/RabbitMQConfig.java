6075204ac919e7587b419c5b433edb69f481f35d
Nitin Bokde <nitinbokde@Nitins-MacBook-Air.local>
2026-04-23 12:36:22 +0530

    feat: setup microservices infrastructure
    • Added Spring Cloud Gateway as API Gateway
    • Configured Eureka Server for service discovery
    • Registered services using Eureka Client
    • Downgraded Spring Boot to 3.2.5 for Spring Cloud compatibility

--- product-service/src/main/java/com/nitin/product_service/config/RabbitMQConfig.java
+++ product-service/src/main/java/com/nitin/product_service/config/RabbitMQConfig.java
@@ -2,7 +2,7 @@ package com.nitin.product_service.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +32,6 @@ public class RabbitMQConfig {
 
      @Bean
      public MessageConverter messageConverter(){
-         return new JacksonJsonMessageConverter();
+         return new Jackson2JsonMessageConverter();
      }
 }
